5565d47c8c012b33612c037fb9e5a942d872b8d2
Gajsilva <devgabrielsj@gmail.com>
2024-02-13 21:08:07 -0300

    Initial commit

---
+++ src/main/java/com/br/swiftdispath/SwiftDispathApplication.java
@@ -0,0 +1,13 @@
+package com.br.swiftdispath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftDispathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftDispathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/swiftdispath/SwiftDispathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.swiftdispath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftDispathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
